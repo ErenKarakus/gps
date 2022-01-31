a92c9b9962f824b96d885e9ca13d50db1fa97d59
Neil Sculthorpe <neil.sculthorpe@ntu.ac.uk>
2022-01-31 11:43:53 +0000

    Implemented hasValidSentenceStructure()

--- src/nmea/nmea-parser.cpp
+++ src/nmea/nmea-parser.cpp
@@ -11,9 +11,57 @@ namespace GPS::NMEA
       return false;
   }
 
-  bool hasValidSentenceStructure(std::string)
+  bool hasValidSentenceStructure(std::string s)
   {
-      // Stub definition, needs implementing
+      char c;
+      unsigned int i;
+      if (s.empty()) return false; // Need this or next line could crash
+      if (s[0] != '$') return false;
+      if (s.size() < 3) return false; // Need this or next line could crash
+      if (s[1] != 'G') return false;
+      if (s[2] != 'P') return false;
+      // cout << "Debug A" << endl;
+      if (s.size() < 6) return false; // Need this or next line could crash
+      c = s[3];
+      if (c < 65 || c > 90) return false;
+      c = s[4];
+      if (c < 65 || c > 90) return false;
+      c = s[5];
+      if (c < 'A' || c > 'Z') return false; // Hey Ken: we can do it like this instead of using the ASCII codes.  Shall I change the previous two like this as well?
+      // cout << "Debug B" << endl;
+      if (s.size() < 7) return false; // Need this or next line could crash.
+      if (s[6] != ',') return false;
+      // cout << "Debug C" << endl;
+      /*
+      i = 7;
+      while (true)
+      {
+          if (i == potentialSentence.size()) return false;
+          if (potentialSentence[i] == '$') return false; // Not allowed here.
+          if (potentialSentence[i] == '*') break;
+          ++i;
+      }
+      */
+      // Nadia: for-loop is better.
+      for (i = 7; true; ++i)
+      {
+          if (i == s.size()) return false;
+          if (s[i] == '$') return false; // Not allowed here.
+          if (s[i] == '*') break;
+      }
+      // cout << "Debug D" << endl;
+      if (s.size() < i+3) return false; // Note: i+3 NOT i+2.  This is confusing.
+      c = s[i+1];
+      if ((c < 48) || (c > 57 && c < 65) || (c > 70 && c < 97) || (c > 102)) return false;
+      c = s[i+2];
+      if ((c < 48) || (c > 57 && c < 65) || (c > 70 && c < 97) || (c > 102)) return false;
+      // cout << "Debug E" << endl;
+      // cout << potentialSentence.size() << endl;
+      // cout << i << endl;
+      if (s.size() != i+3) return false; // Note: i+3 NOT i+2.  This is confusing.
+      // Alice: It's because indexing starts at 0, so the index number is one less.
+      // Nadia: No, it's because we're using '<' rather than '<='.  We need a char at i+2, so we say < i+3.
+      // Ken: But indexing normally starts at 0, and we normally use <. So why is this different?
       return true;
   }
 
