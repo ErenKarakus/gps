a0059bf11624fa87715bb2f4999bc8907b3cfea5
ErenKarakus <73061472+ErenKarakus@users.noreply.github.com>
2022-03-10 22:30:28 +0000

    created new function sentenceBearing which is being used for positionFromSentenceData function. completed readSentences function

--- src/nmea/nmea-parser.cpp
+++ src/nmea/nmea-parser.cpp
@@ -8,226 +8,157 @@ namespace GPS::NMEA
 {
   bool isSupportedFormat(std::string characterFormat)
   {
-      // Stub definition, needs implementing
+      //Regex to determine whether the parameter is the three-character code for sentence format
       const std::regex reg("(GLL|GGA|RMC)");
       return std::regex_match (characterFormat, reg);
   }
 
-  bool hasValidSentenceStructure(std::string s)
+  bool hasValidSentenceStructure(std::string sentence)
   {
-      std::regex expression("(\\$GP)([A-Z]){3},(\\w|[.]|[,]){0,}\\*[[:xdigit:]]{2}");
-      return std::regex_match(s, expression);
+      //Regex to determine whether the sentence conforms to the structure of NMEA sentence
+      std::regex expression("(\\$GP)([A-Z]){3},(\\w|[-]|[.]|[,]){0,}\\*[[:xdigit:]]{2}");
+      return std::regex_match(sentence, expression);
   }
 
-  bool checksumMatches(std::string s)
+  bool checksumMatches(std::string sentence)
   {
-      // Stub definition, needs implementing
       int baseHex = 16;
-      int endPoint = s.length() - 2;
+      int endPoint = sentence.length() - 2;
 
-      std::string subString = s.substr(endPoint);
+      //Obtains checksum provided from sentence
+      std::string subString = sentence.substr(endPoint);
       int hexValues = std::stoul(subString, nullptr, baseHex);
 
+      //Calculation of new bitwise checksum, via XOR
       int checksum = 0;
       for(int i = 1; i < endPoint - 1; i++) {
-          checksum ^= s[i];
+          checksum ^= sentence[i];
       }
-
       return (checksum == hexValues);
   }
 
-  SentenceData parseSentence(std::string s)
+  SentenceData parseSentence(std::string sentence)
   {
-      // Stub definition, needs implementing
-      std::string format = s.substr(3, 3);
+      //Creation of string to take sentece format, with: starting position 3 and length of 3 characters
+      std::string sFormat = sentence.substr(3, 3);
 
-      std::vector<std::string> field;
-      std::string data = "";
+      //Creation of vector for Data Fields
+      std::vector<std::string> dataFields;
+      std::string format = "";
 
-      for (int i =7 ;i <s.length() - 2; i++) {
-          char contents = s[i];
+      //Loop that pushes fields into vector value
+      //Finds occurance of `,` and end of Data Fields
+      for (unsigned int i =7 ;i <sentence.length() - 2; i++) {
+          char contents = sentence[i];
           if((contents == ',')|| (contents == '*')){
-              field.push_back(data);
-              data = "";
+              dataFields.push_back(format);
+              format = "";
           }
           else {
-              data = data + contents;
+              format = format + contents;
           }
-
       }
-      return {format, field};
+      return {sFormat, dataFields};
   }
 
-  bool hasCorrectNumberOfFields(SentenceData sentenceFields)
+  bool hasCorrectNumberOfFields(SentenceData sentenceData)
   {
-      // Stub definition, needs implementing
-      int fieldAmount = sentenceFields.dataFields.size();
+      int fieldAmount = sentenceData.dataFields.size();
 
-      if (sentenceFields.format == "GLL" && fieldAmount == 5){
+      if (sentenceData.format == "GLL" && fieldAmount == 5){
           return true;
       }
-      else if (sentenceFields.format == "GGA" && fieldAmount == 14) {
+      else if (sentenceData.format == "GGA" && fieldAmount == 14) {
           return true;
       }
-      else if (sentenceFields.format == "RMC" && fieldAmount ==11) {
+      else if (sentenceData.format == "RMC" && fieldAmount ==11) {
           return true;
       }
       else {
           return false;
       }
-
   }
+
+  void sentenceBearing (std::string northSouth, std::string eastWest)
+  {
+      if (northSouth.size() != 1) {
+            throw std::domain_error(std::string("Ill-formed bearing in GLL sentence field: ") + northSouth + ". Bearings must be a single character.");
+      }
+          else if (eastWest.size() != 1) {
+              throw std::domain_error(std::string("Ill-formed bearing in GLL sentence field: ") + eastWest + ". Bearings must be a single character.");
+          }
+  }
+
 
   Position positionFromSentenceData(SentenceData d)
   {
-      //using namespace std; // Ken: Writing 'std::' everywhere is irritating.
       std::string latitude, longitude, northSouth, eastWest, elevation;
-      char northSouth0, eastWest0;
-      Position p = Position(0,0,0); // Dummy object becasue there's no default constructor available for Postion
+      Position p = Position(0,0,0);
 
-      if (d.format == "GLL")
+      if (d.format == "GLL" && d.dataFields.size() == 5)
       {
           latitude = d.dataFields[0];
           longitude = d.dataFields[2];
           northSouth = d.dataFields[1];
           eastWest = d.dataFields[3];
-          elevation = "0"; // no ele
-
-          if (northSouth.size() == 1) {
-              if (northSouth == "N") {
-                  northSouth0 = northSouth[0];
-              }
-              else if (northSouth == "S") {
-                  northSouth0 = northSouth[0];
-              }
-              else {
-                  throw std::domain_error(std::string("Ill-formed bearing in GLL sentence field: ") + northSouth + ". Bearings must be N or S.");
-              }
-          }
-
-          else {
-              throw std::domain_error(std::string("Ill-formed bearing in GLL sentence field: ") + northSouth + ". Bearings must be a single character.");
-          }
-
-          if (eastWest.size() == 1) {
-              if (eastWest == "E") {
-                  eastWest0 = eastWest[0];
-              }
-              else if (eastWest == "W") {
-                  eastWest0 = eastWest[0];
-              }
-              else {
-                   throw std::domain_error(std::string("Ill-formed bearing in GLL sentence field: ") + eastWest + ". Bearings must be E or W.");
-              }
-          }
-
-          else {
-              throw std::domain_error(std::string("Ill-formed bearing in GLL sentence field: ") + eastWest + ". Bearings must be a single character.");
-          }
-          try {
-              p = Position(latitude,northSouth0,longitude,eastWest0,elevation);
-          } catch (const std::invalid_argument& e) {
-              throw std::domain_error(std::string("Ill-formed GLL sentence field: ") + e.what());
-          }
+          elevation = "0";
       }
 
-      else if (d.format == "RMC")
-      {
-          latitude = d.dataFields[2];
-          longitude = d.dataFields[4];
-          northSouth = d.dataFields[3];
-          eastWest = d.dataFields[5];
-          elevation = "0"; // no ele
-
-          if (northSouth.size() == 1) {
-              if (northSouth == "N") {
-                  northSouth0 = northSouth[0];
-              }
-              else if (northSouth == "S") {
-                  northSouth0 = northSouth[0];
-              }
-              else {
-                  throw std::domain_error(std::string("Ill-formed bearing in RMC sentence field: ") + northSouth + ". Bearings must be N or S.");
-              }
-          }
-
-          else {
-              throw std::domain_error(std::string("Ill-formed bearing in RMC sentence field: ") + northSouth + ". Bearings must be a single character.");
-          }
-
-          if (eastWest.size() == 1) {
-              if (eastWest == "E") {
-                  eastWest0 = eastWest[0];
-              }
-              else if (eastWest == "W") {
-                  eastWest0 = eastWest[0];
-              }
-              else {
-                  throw std::domain_error(std::string("Ill-formed bearing in RMC sentence field: ") + eastWest + ". Bearings must be E or W.");
-              }
-          }
-          else {
-              throw std::domain_error(std::string("Ill-formed bearing in RMC sentence field: ") + eastWest + ". Bearings must be a single character.");
-          }
-              try {
-                  p = Position(latitude,northSouth0,longitude,eastWest0,elevation);
-              } catch (const std::invalid_argument& e) {
-                  throw std::domain_error(std::string("Ill-formed RMC sentence field: ") + e.what());
-              }
-      }
-
-      else if (d.format == "GGA")
-      {
+      else if (d.format == "GGA" && d.dataFields.size() == 14) {
           latitude = d.dataFields[1];
           longitude = d.dataFields[3];
           northSouth = d.dataFields[2];
           eastWest = d.dataFields[4];
           elevation = d.dataFields[8];
-          if (northSouth.size() == 1) {
-              if (northSouth == "N") {
-                  northSouth = northSouth[0];
-              }
-              else if (northSouth == "S") {
-                  northSouth = northSouth[0];
-              }
-              else {
-                  throw std::domain_error(std::string("Ill-formed bearing in GGA sentence field: ") + northSouth + ". Bearings must be N or S.");
-              }
-          }
-
-          else {
-              throw std::domain_error(std::string("Ill-formed bearing in GGA sentence field: ") + northSouth + ". Bearings must be a single character.");
-          }
-
-          if (eastWest.size() == 1) {
-              if (eastWest == "E") {
-                  eastWest0 = eastWest[0];
-              }
-              else if (eastWest == "W") {
-                  eastWest0 = eastWest[0];
-              }
-              else {
-                  throw std::domain_error(std::string("Ill-formed bearing in GGA sentence field: ") + eastWest + ". Bearings must be E or W.");
-              }
-          }
-
-          else {
-              throw std::domain_error(std::string("Ill-formed bearing in GGA sentence field: ") + eastWest + ". Bearings must be a single character.");
-          }
-          eastWest0 = eastWest[0];
-          try {
-              p = Position(latitude,northSouth0,longitude,eastWest0,elevation);
-          } catch (const std::invalid_argument& e)
-          {
-              throw std::domain_error(std::string("Ill-formed GGA sentence field: ") + e.what());
-          }
       }
-      return p;
+      else if (d.format == "RMC" && d.dataFields.size() ==11) {
+          latitude = d.dataFields[2];
+          longitude = d.dataFields[4];
+          northSouth = d.dataFields[3];
+          eastWest = d.dataFields[5];
+          elevation = "0";
+      }
+      else {
+          throw std::domain_error(std::string("Ill-formed bearing in sentence field: "));
+      }
+
+      sentenceBearing(northSouth, eastWest);
+
+      try {
+          p = Position(latitude,northSouth[0],longitude,eastWest[0],elevation);
+      } catch (const std::invalid_argument& e) {
+          throw std::domain_error(std::string("Ill-formed GLL sentence field: ") + e.what());
+      }
+    return p;
   }
 
-  std::vector<Position> readSentences(std::istream &)
+  std::vector<Position> readSentences(std::istream & stream)
   {
-      // Stub definition, needs implementing
-      return {};
+      std::vector<Position> vector;
+
+      while (true) {
+          std::string validData;
+          stream >> validData;
+          if (stream.eof()) {
+              break;
+          }
+          if (!validData.length()) {
+              continue;
+          }
+
+          //Checks line is valid by meeting five conditons
+          try {
+              if(!hasValidSentenceStructure(validData)) {
+                  throw std::invalid_argument("Parameter doesn't conform to the structure of a NMEA sentence");
+              }
+              SentenceData sentence (parseSentence(validData));
+
+              if((isSupportedFormat(sentence.format))&&(hasValidSentenceStructure(validData))&&(checksumMatches(validData))) {
+                  vector.push_back(positionFromSentenceData(sentence));
+              }
+          } catch (const std::exception& ) {
+          }
+      }
+      return vector;
   }
 }
