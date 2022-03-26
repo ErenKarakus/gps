aa259243683124c659e93e9ce4da386fcb16b4d9
Neil Sculthorpe <neil.sculthorpe@ntu.ac.uk>
2022-03-26 15:43:37 +0000

    Added XML Generator

---
+++ headers/xml/xml-generator.h
@@ -0,0 +1,45 @@
+#ifndef XML_GENERATOR_H
+#define XML_GENERATOR_H
+
+#include <sstream>
+#include <string>
+#include <stack>
+
+#include "xml-element.h"
+
+namespace XML
+{
+  class Generator
+  {
+    public:
+      Generator(unsigned int indentationSpaces = 4);
+
+      void basicXMLDeclaration();
+      void openBasicGPXElement();
+
+      void element(const ElementName&, const Attributes&, const LeafContent&);
+      void openElement(const ElementName&, const Attributes&);
+      void closeElement();
+      void closeAllElements();
+
+      std::string closeAllElementsAndExtractString();
+
+    private:
+      std::ostringstream xml;
+      std::stack<std::string> unclosedTags;
+      unsigned int indentationSpaces;
+      unsigned int indentationLevel = 0;
+
+      void openingTag(const ElementName&, const Attributes&);
+      void closingTag(const ElementName&);
+
+      void attributes(const Attributes&);
+      void attribute(const AttributeName&, const AttributeValue&);
+
+      void indent();
+      void newline();
+  };
+
+}
+
+#endif
