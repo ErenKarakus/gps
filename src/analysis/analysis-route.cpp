cb3a9a440a4a2610477c655c980af8db060b796a
Neil Sculthorpe <neil.sculthorpe@ntu.ac.uk>
2022-03-26 15:27:51 +0000

    Implemented all analysis functions

--- src/analysis/analysis-route.cpp
+++ src/analysis/analysis-route.cpp
@@ -1,6 +1,7 @@
+#include <cmath>
+#include <algorithm>
 #include <stdexcept>
 
-#include "earth.h"
 #include "geometry.h"
 
 #include "analysis-route.h"
@@ -8,18 +9,13 @@
 namespace GPS::Analysis
 {
 
-///////////////////////////////////////////////////////////////////////////////
-
 Route::Route(std::vector<RoutePoint> routePoints)
     : routePoints{routePoints}
 {}
 
-///////////////////////////////////////////////////////////////////////////////
-
 unsigned int Route::numPoints() const
 {
-    // Stub definition, needs implementing
-    return 0;
+    return routePoints.size();
 }
 
 metres Route::totalLength() const
@@ -40,124 +36,303 @@ metres Route::totalLength() const
 
 metres Route::netLength() const
 {
-    // Stub definition, needs implementing
-    return 0;
+    if (routePoints.empty()) throw std::domain_error("Cannot compute the length of an empty route.");
+
+    metres horizontalDifference = Position::horizontalDistanceBetween(routePoints.front().position, routePoints.back().position);
+    metres verticalDifference = routePoints.back().position.elevation() - routePoints.front().position.elevation();
+    return pythagoras(horizontalDifference,verticalDifference);
 }
 
 metres Route::totalHeightGain() const
 {
-    // Stub definition, needs implementing
-    return 0;
+    if (routePoints.empty()) throw std::domain_error("Cannot compute the height gain of an empty route.");
+
+    metres total = 0;
+
+    for (unsigned int current = 0, next = 1; next < routePoints.size() ; ++current, ++next)
+    {
+        metres verticalDifference = routePoints[next].position.elevation() - routePoints[current].position.elevation();
+        if (verticalDifference > 0) total += verticalDifference; // ignore negative height differences
+    }
+
+    return total;
 }
 
 metres Route::netHeightGain() const
 {
-    // Stub definition, needs implementing
-    return 0;
+    if (routePoints.empty()) throw std::domain_error("Cannot compute the height gain of an empty route.");
+
+    metres verticalDifference = routePoints.back().position.elevation() - routePoints.front().position.elevation();
+    return std::max(verticalDifference,0.0); // ignore negative height differences
 }
 
 degrees Route::maxGradient() const
 {
-    // Stub definition, needs implementing
-    return 0;
+    if (routePoints.size() < 2) throw std::domain_error("Cannot compute gradients on a route of fewer than two points.");
+
+    degrees maxGrad = -halfRotation/2; // minimum possible gradient value
+
+    for (unsigned int current = 0, next = 1; next < routePoints.size() ; ++current, ++next)
+    {
+        metres horizontalDifference = Position::horizontalDistanceBetween(routePoints[current].position, routePoints[next].position);
+        metres verticalDifference = routePoints[next].position.elevation() - routePoints[current].position.elevation();
+        degrees grad = radToDeg(std::atan(verticalDifference/horizontalDifference));
+        maxGrad = std::max(maxGrad,grad);
+    }
+
+    return maxGrad;
 }
 
 degrees Route::minGradient() const
 {
-    // Stub definition, needs implementing
-    return 0;
+    if (routePoints.size() < 2) throw std::domain_error("Cannot compute gradients on a route of fewer than two points.");
+
+    degrees minGrad = halfRotation/2; // maximum possible gradient value
+
+    for (unsigned int current = 0, next = 1; next < routePoints.size() ; ++current, ++next)
+    {
+        metres horizontalDifference = Position::horizontalDistanceBetween(routePoints[current].position, routePoints[next].position);
+        metres verticalDifference = routePoints[next].position.elevation() - routePoints[current].position.elevation();
+        degrees grad = radToDeg(std::atan(verticalDifference/horizontalDifference));
+        minGrad = std::min(minGrad,grad);
+    }
+
+    return minGrad;
 }
 
 degrees Route::steepestGradient() const
 {
-    // Stub definition, needs implementing
-    return 0;
+    if (routePoints.size() < 2) throw std::domain_error("Cannot compute gradients on a route of fewer than two points.");
+
+    degrees steepestGrad = 0; // minimum possible gradient value
+
+    for (unsigned int current = 0, next = 1; next < routePoints.size() ; ++current, ++next)
+    {
+        metres horizontalDifference = Position::horizontalDistanceBetween(routePoints[current].position, routePoints[next].position);
+        metres verticalDifference = routePoints[next].position.elevation() - routePoints[current].position.elevation();
+        degrees grad = radToDeg(std::atan(verticalDifference/horizontalDifference));
+        if (std::abs(grad) > std::abs(steepestGrad))
+        {
+            steepestGrad = grad;
+        }
+    }
+    return steepestGrad;
 }
 
 RoutePoint Route::highestPoint() const
 {
-    // Stub definition, needs implementing
-    return RoutePoint{ Earth::CliftonCampus, "Clifton Campus" };
+    if (routePoints.empty()) throw std::domain_error("Cannot locate a point in an empty route.");
+
+    RoutePoint highestPointSoFar = routePoints.front();
+    for (const RoutePoint& currentPoint : routePoints)
+    {
+        if (currentPoint.position.elevation() > highestPointSoFar.position.elevation())
+        {
+            highestPointSoFar = currentPoint;
+        }
+    }
+    return highestPointSoFar;
 }
 
 RoutePoint Route::lowestPoint() const
 {
-    // Stub definition, needs implementing
-    return RoutePoint{ Earth::CliftonCampus, "Clifton Campus" };
+    if (routePoints.empty()) throw std::domain_error("Cannot locate a point in an empty route.");
+
+    RoutePoint lowestPointSoFar = routePoints.front();
+    for (const RoutePoint& currentPoint : routePoints)
+    {
+        if (currentPoint.position.elevation() < lowestPointSoFar.position.elevation())
+        {
+            lowestPointSoFar = currentPoint;
+        }
+    }
+    return lowestPointSoFar;
 }
 
 RoutePoint Route::mostNorthelyPoint() const
 {
-    // Stub definition, needs implementing
-    return RoutePoint{ Earth::CliftonCampus, "Clifton Campus" };
+    if (routePoints.empty()) throw std::domain_error("Cannot locate a point in an empty route.");
+
+    RoutePoint northmostPointSoFar = routePoints.front();
+    for (const RoutePoint& currentPoint : routePoints)
+    {
+        if (currentPoint.position.latitude() > northmostPointSoFar.position.latitude())
+        {
+            northmostPointSoFar = currentPoint;
+        }
+    }
+    return northmostPointSoFar;
 }
 
 RoutePoint Route::mostSoutherlyPoint() const
 {
-    // Stub definition, needs implementing
-    return RoutePoint{ Earth::CliftonCampus, "Clifton Campus" };
+    if (routePoints.empty()) throw std::domain_error("Cannot locate a point in an empty route.");
+
+    RoutePoint southmostPointSoFar = routePoints.front();
+    for (const RoutePoint& currentPoint : routePoints)
+    {
+        if (currentPoint.position.latitude() < southmostPointSoFar.position.latitude())
+        {
+            southmostPointSoFar = currentPoint;
+        }
+    }
+    return southmostPointSoFar;
 }
 
 RoutePoint Route::mostEasterlyPoint() const
 {
-    // Stub definition, needs implementing
-    return RoutePoint{ Earth::CliftonCampus, "Clifton Campus" };
+    if (routePoints.empty()) throw std::domain_error("Cannot locate a point in an empty route.");
+
+    RoutePoint eastmostPointSoFar = routePoints.front();
+    for (const RoutePoint& currentPoint : routePoints)
+    {
+        if (currentPoint.position.longitude() > eastmostPointSoFar.position.longitude())
+        {
+            eastmostPointSoFar = currentPoint;
+        }
+    }
+    return eastmostPointSoFar;
 }
 
 RoutePoint Route::mostWesterlyPoint() const
 {
-    // Stub definition, needs implementing
-    return RoutePoint{ Earth::CliftonCampus, "Clifton Campus" };
+    if (routePoints.empty()) throw std::domain_error("Cannot locate a point in an empty route.");
+
+    RoutePoint westmostPointSoFar = routePoints.front();
+    for (const RoutePoint& currentPoint : routePoints)
+    {
+        if (currentPoint.position.longitude() < westmostPointSoFar.position.longitude())
+        {
+            westmostPointSoFar = currentPoint;
+        }
+    }
+    return westmostPointSoFar;
 }
 
 RoutePoint Route::mostEquatorialPoint() const
 {
-    // Stub definition, needs implementing
-    return RoutePoint{ Earth::CliftonCampus, "Clifton Campus" };
+    if (routePoints.empty()) throw std::domain_error("Cannot locate a point in an empty route.");
+
+    RoutePoint nearestPointSoFar = routePoints.front();
+    for (const RoutePoint& currentPoint : routePoints)
+    {
+        if (std::abs(currentPoint.position.latitude()) < std::abs(nearestPointSoFar.position.latitude()))
+        {
+            nearestPointSoFar = currentPoint;
+        }
+    }
+    return nearestPointSoFar;
 }
 
 RoutePoint Route::leastEquatorialPoint() const
 {
-    // Stub definition, needs implementing
-    return RoutePoint{ Earth::CliftonCampus, "Clifton Campus" };
+    if (routePoints.empty()) throw std::domain_error("Cannot locate a point in an empty route.");
+
+    RoutePoint farthestPointSoFar = routePoints.front();
+    for (const RoutePoint& currentPoint : routePoints)
+    {
+        if (std::abs(currentPoint.position.latitude()) > std::abs(farthestPointSoFar.position.latitude()))
+        {
+            farthestPointSoFar = currentPoint;
+        }
+    }
+    return farthestPointSoFar;
 }
 
-RoutePoint Route::nearestPointTo(Position) const
+RoutePoint Route::nearestPointTo(Position targetPosition) const
 {
-    // Stub definition, needs implementing
-    return RoutePoint{ Earth::CliftonCampus, "Clifton Campus" };
+    if (routePoints.empty()) throw std::domain_error("Cannot locate nearest point in an empty route.");
+
+    RoutePoint nearestPointSoFar = routePoints.front();
+    metres shortestDistanceSoFar = Position::horizontalDistanceBetween(nearestPointSoFar.position, targetPosition);
+
+    for (const RoutePoint& currentPoint : routePoints)
+    {
+        metres horizontalDifference = Position::horizontalDistanceBetween(currentPoint.position, targetPosition);
+        metres verticalDifference = currentPoint.position.elevation() - targetPosition.elevation();
+        metres currentDistance = pythagoras(horizontalDifference,verticalDifference);
+        if (currentDistance < shortestDistanceSoFar)
+        {
+            nearestPointSoFar = currentPoint;
+            shortestDistanceSoFar = currentDistance;
+        }
+    }
+
+    return nearestPointSoFar;
 }
 
-RoutePoint Route::farthestPointFrom(Position) const
+RoutePoint Route::farthestPointFrom(Position avoidedPosition) const
 {
-    // Stub definition, needs implementing
-    return RoutePoint{ Earth::CliftonCampus, "Clifton Campus" };
+    if (routePoints.empty()) throw std::domain_error("Cannot locate farthest point in an empty route.");
+
+    RoutePoint farthestPointSoFar = routePoints.front();
+    metres longestDistanceSoFar = Position::horizontalDistanceBetween(farthestPointSoFar.position, avoidedPosition);
+
+    for (const RoutePoint& currentPoint : routePoints)
+    {
+        metres horizontalDifference = Position::horizontalDistanceBetween(currentPoint.position, avoidedPosition);
+        metres verticalDifference = currentPoint.position.elevation() - avoidedPosition.elevation();
+        metres currentDistance = pythagoras(horizontalDifference,verticalDifference);
+        if (currentDistance > longestDistanceSoFar)
+        {
+            farthestPointSoFar = currentPoint;
+            longestDistanceSoFar = currentDistance;
+        }
+    }
+
+    return farthestPointSoFar;
 }
 
-unsigned int Route::numPointsNear(Position, metres) const
+unsigned int Route::numPointsNear(Position targetPosition, metres nearDistance) const
 {
-    // Stub definition, needs implementing
-    return 0;
+    unsigned int numNear = 0;
+
+    for (const RoutePoint& routePoint : routePoints)
+    {
+        metres horizontalDifference = Position::horizontalDistanceBetween(routePoint.position, targetPosition);
+        metres verticalDifference = routePoint.position.elevation() - targetPosition.elevation();
+        metres currentDistance = pythagoras(horizontalDifference,verticalDifference);
+        if (currentDistance < nearDistance) ++numNear;
+    }
+
+    return numNear;
 }
 
-unsigned int Route::timesVisited(std::string) const
+unsigned int Route::timesVisited(std::string soughtName) const
 {
-    // Stub definition, needs implementing
-    return 0;
+    if (soughtName.empty()) throw std::invalid_argument("The name of the route point to be visited must be provided.");
+
+    unsigned int numberOfVisits = 0;
+
+    for (const RoutePoint& routePoint : routePoints)
+    {
+        if (routePoint.name == soughtName) ++numberOfVisits;
+    }
+
+    return numberOfVisits;
 }
 
-Position Route::findPosition(std::string) const
+Position Route::findPosition(std::string soughtName) const
 {
-    // Stub definition, needs implementing
-    return Earth::CliftonCampus;
+    if (soughtName.empty()) throw std::invalid_argument("Cannot find the position of an empty name.");
+
+    for (const RoutePoint& routePoint : routePoints)
+    {
+        if (routePoint.name == soughtName) return routePoint.position;
+    }
+
+    throw std::domain_error("No position with that name found in the route.");
 }
 
-RoutePoint Route::operator[](unsigned int) const
+RoutePoint Route::operator[](unsigned int index) const
 {
-    // Stub definition, needs implementing
-    return RoutePoint{ Earth::CliftonCampus, "Clifton Campus" };
+    if (index >= routePoints.size())
+    {
+        throw std::out_of_range("Position index out-of-range.");
+    }
+
+    return routePoints[index];
 }
 
-///////////////////////////////////////////////////////////////////////////////
 
 }
--- src/analysis/analysis-track.cpp
+++ src/analysis/analysis-track.cpp
@@ -1,4 +1,6 @@
 #include <cassert>
+#include <cmath>
+#include <algorithm>
 #include <stdexcept>
 
 #include "geometry.h"
@@ -11,8 +13,6 @@ using std::chrono::duration_cast;
 namespace GPS::Analysis
 {
 
-///////////////////////////////////////////////////////////////////////////////
-
 Track::Track(std::vector<TrackPoint> trackPoints, metres restingRange)
     : Route{trackPointsToRoutePoints(trackPoints)},
       timeStamps{trackPointsToTimeStamps(trackPoints)},
@@ -21,14 +21,10 @@ Track::Track(std::vector<TrackPoint> trackPoints, metres restingRange)
     assert (routePoints.size() == timeStamps.size());
 }
 
-///////////////////////////////////////////////////////////////////////////////
-
 void Track::setRestingRange(metres newRestingRange)
 {
     restingRange = newRestingRange;
 }
-
-///////////////////////////////////////////////////////////////////////////////
 
 seconds Track::totalTime() const
 {
@@ -41,71 +37,291 @@ seconds Track::totalTime() const
 
 seconds Track::restingTime() const
 {
-    // Stub definition, needs implementing
-    return seconds::zero();
+    if (timeStamps.empty()) throw std::domain_error("Cannot compute the resting time of an empty track.");
+
+    seconds total = seconds::zero();
+
+    for (unsigned int current = 0, next = 1; next < timeStamps.size() ; ++current, ++next)
+    {
+        if (withinRestingRange(routePoints[current], routePoints[next]))
+        {
+            if (timeStamps[next] < timeStamps[current]) throw std::domain_error("Track contains rests of negative time duration.");
+
+            total += duration_cast<seconds>(timeStamps[next] - timeStamps[current]);
+        }
+    }
+    return total;
 }
 
 seconds Track::travellingTime() const
 {
-    // Stub definition, needs implementing
-    return seconds::zero();
+    if (timeStamps.empty()) throw std::domain_error("Cannot compute the travelling time of an empty track.");
+
+    seconds total = seconds::zero();
+
+    for (unsigned int current = 0, next = 1; next < timeStamps.size() ; ++current, ++next)
+    {
+        if (! withinRestingRange(routePoints[current], routePoints[next]))
+        {
+            if (timeStamps[next] < timeStamps[current]) throw std::domain_error("Track contains travelling with negative time duration.");
+
+            total += duration_cast<seconds>(timeStamps[next] - timeStamps[current]);
+        }
+    }
+    return total;
 }
 
 seconds Track::longestRestingPeriod() const
 {
-    // Stub definition, needs implementing
-    return seconds::zero();
+    if (timeStamps.empty()) throw std::domain_error("Cannot find resting periods in an empty track.");
+
+    seconds maxRest = seconds::zero();
+
+    seconds currentRest = seconds::zero();
+
+    for (unsigned int current = 0, next = 1; next < timeStamps.size() ; ++current, ++next)
+    {
+        if (withinRestingRange(routePoints[current], routePoints[next]))
+        {
+            if (timeStamps[next] < timeStamps[current]) throw std::domain_error("Track contains rests of negative time duration.");
+
+            currentRest += duration_cast<seconds>(timeStamps[next] - timeStamps[current]);
+        }
+        else
+        {
+            // We may have just finished a resting period, so check and reset.
+            maxRest = std::max(maxRest,currentRest);
+            currentRest = seconds::zero();
+        }
+    }
+
+    // If there is a resting period at the end of the Track, then that period will not have been checked in the loop, so we check it here.
+    maxRest = std::max(maxRest,currentRest);
+
+    return maxRest;
 }
 
 std::chrono::seconds Track::longestTravellingPeriod() const
 {
-    // Stub definition, needs implementing
-    return seconds::zero();
+    if (timeStamps.empty()) throw std::domain_error("Cannot find travelling periods in an empty track.");
+
+    seconds maxTravelling = seconds::zero();
+
+    seconds currentTravelling = seconds::zero();
+
+    for (unsigned int current = 0, next = 1; next < timeStamps.size() ; ++current, ++next)
+    {
+        if (! withinRestingRange(routePoints[current], routePoints[next]))
+        {
+            if (timeStamps[next] < timeStamps[current]) throw std::domain_error("Track contains travelling of negative time duration.");
+
+            currentTravelling += duration_cast<seconds>(timeStamps[next] - timeStamps[current]);
+        }
+        else
+        {
+            // We may have just finished a travelling period, so check and reset.
+            maxTravelling = std::max(maxTravelling,currentTravelling);
+            currentTravelling = seconds::zero();
+        }
+    }
+
+    // If there is a travelling period at the end of the Track, then that period will not have been checked in the loop, so we check it here.
+    maxTravelling = std::max(maxTravelling,currentTravelling);
+
+    return maxTravelling;
 }
 
 seconds Track::averageRestingPeriod() const
 {
-    // Stub definition, needs implementing
-    return seconds::zero();
+    if (timeStamps.empty()) throw std::domain_error("Cannot find resting periods in an empty track.");
+
+    int numRestingPeriods = 0;
+    bool currentlyResting = false;
+
+    for (unsigned int current = 0, next = 1; next < timeStamps.size() ; ++current, ++next)
+    {
+        if (withinRestingRange(routePoints[current], routePoints[next]))
+        {
+            currentlyResting = true;
+        }
+        else if (currentlyResting)
+        {
+            ++numRestingPeriods;
+            currentlyResting = false;
+        }
+    }
+
+    if (currentlyResting) ++numRestingPeriods;
+
+    return restingTime() / numRestingPeriods;
 }
 
 seconds Track::averageTravellingPeriod() const
 {
-    // Stub definition, needs implementing
-    return seconds::zero();
+    if (timeStamps.empty()) throw std::domain_error("Cannot find travelling periods in an empty track.");
+
+    int numTravellingPeriods = 0;
+    bool currentlyTravelling = false;
+
+    for (unsigned int current = 0, next = 1; next < timeStamps.size() ; ++current, ++next)
+    {
+        if (! withinRestingRange(routePoints[current], routePoints[next]))
+        {
+            currentlyTravelling = true;
+        }
+        else if (currentlyTravelling)
+        {
+            ++numTravellingPeriods;
+            currentlyTravelling = false;
+        }
+    }
+
+    if (currentlyTravelling) ++numTravellingPeriods;
+
+    return travellingTime() / numTravellingPeriods;
 }
 
 speed Track::maxSpeed() const
 {
-    // Stub definition, needs implementing
-    return 0;
+    if (timeStamps.empty()) throw std::domain_error("Cannot measure speed in an empty track.");
+
+    assert( routePoints.size() == timeStamps.size() );
+
+    speed maximumSpeed = 0;
+
+    for (unsigned int current = 0, next = 1; next < timeStamps.size() ; ++current, ++next)
+    {
+        if (! withinRestingRange(routePoints[current], routePoints[next]))
+        {
+            if (timeStamps[next] < timeStamps[current]) throw std::domain_error("Track contains travelling of negative time duration.");
+
+            seconds timeDifference = duration_cast<seconds>(timeStamps[next] - timeStamps[current]);
+
+            if (timeDifference == seconds::zero()) throw std::domain_error("Cannot compute maximum speed in m/s when there is travelling of zero seconds duration.");
+
+            Position currentPos = routePoints[current].position;
+            Position nextPos = routePoints[next].position;
+
+            metres horizontalDifference = Position::horizontalDistanceBetween(currentPos,nextPos);
+            metres verticalDifference = nextPos.elevation() - currentPos.elevation();
+
+            metres distance = pythagoras(horizontalDifference,verticalDifference);
+
+            speed currentSpeed = distance / timeDifference.count();
+
+            maximumSpeed =  std::max(currentSpeed,maximumSpeed);
+        }
+    }
+
+    return maximumSpeed;
 }
 
 speed Track::averageSpeed() const
 {
-    // Stub definition, needs implementing
-    return 0;
+    if (timeStamps.empty()) throw std::domain_error("Cannot measure speed in an empty track.");
+
+    seconds timeTaken = totalTime();
+
+    if (timeTaken == seconds::zero()) throw std::domain_error("Cannot speed in m/s when the travel-time has zero seconds duration.");
+
+    return totalLength() / timeTaken.count();
 }
 
 speed Track::averageTravellingSpeed() const
 {
-    // Stub definition, needs implementing
-    return 0;
+    assert( routePoints.size() == timeStamps.size() );
+
+    metres distanceTravelled = 0;
+    seconds timeTravelling = seconds::zero();
+
+    for (unsigned int current = 0, next = 1; next < timeStamps.size() ; ++current, ++next)
+    {
+        if (! withinRestingRange(routePoints[current], routePoints[next]))
+        {
+            if (timeStamps[next] < timeStamps[current]) throw std::domain_error("Track contains travelling of negative time duration.");
+
+            seconds timeDifference = duration_cast<seconds>(timeStamps[next] - timeStamps[current]);
+
+            if (timeDifference == seconds::zero()) throw std::domain_error("Cannot compute average speed in m/s when there is travelling of zero seconds duration.");
+
+            Position currentPos = routePoints[current].position;
+            Position nextPos = routePoints[next].position;
+
+            metres horizontalDifference = Position::horizontalDistanceBetween(currentPos,nextPos);
+            metres verticalDifference = nextPos.elevation() - currentPos.elevation();
+
+            distanceTravelled += pythagoras(horizontalDifference,verticalDifference);
+
+            timeTravelling += timeDifference;
+        }
+    }
+
+    if (timeTravelling == seconds::zero()) throw std::domain_error("Cannot calculate average travelling speed in a track with no travelling periods.");
+
+    return distanceTravelled / timeTravelling.count();
 }
 
 speed Track::maxRateOfAscent() const
 {
-    // Stub definition, needs implementing
-    return 0;
+    if (timeStamps.empty()) throw std::domain_error("Cannot measure rate of ascent in an empty track.");
+
+    assert( routePoints.size() == timeStamps.size() );
+
+    speed maxAscentRate = 0;
+
+    for (unsigned int current = 0, next = 1; next < timeStamps.size() ; ++current, ++next)
+    {
+        if (! withinRestingRange(routePoints[current], routePoints[next]))
+        {
+            if (timeStamps[next] < timeStamps[current]) throw std::domain_error("Track contains travelling of negative time duration.");
+
+            seconds timeDifference = duration_cast<seconds>(timeStamps[next] - timeStamps[current]);
+
+            if (timeDifference == seconds::zero()) throw std::domain_error("Cannot compute maximum rate of ascent in m/s when there is travelling of zero seconds duration.");
+
+            metres verticalDifference = routePoints[next].position.elevation() - routePoints[current].position.elevation();
+
+            if (verticalDifference > 0)
+            {
+                speed currentAscentRate = verticalDifference / timeDifference.count();
+                maxAscentRate = std::max(currentAscentRate,maxAscentRate);
+            }
+        }
+    }
+
+    return maxAscentRate;
 }
 
 speed Track::maxRateOfDescent() const
 {
-    // Stub definition, needs implementing
-    return 0;
-}
+    if (timeStamps.empty()) throw std::domain_error("Cannot measure rate of ascent in an empty track.");
 
-///////////////////////////////////////////////////////////////////////////////
+    assert( routePoints.size() == timeStamps.size() );
+
+    speed maxDescentRate = 0;
+
+    for (unsigned int current = 0, next = 1; next < timeStamps.size() ; ++current, ++next)
+    {
+        if (! withinRestingRange(routePoints[current], routePoints[next]))
+        {
+            if (timeStamps[next] < timeStamps[current]) throw std::domain_error("Track contains travelling of negative time duration.");
+
+            seconds timeDifference = duration_cast<seconds>(timeStamps[next] - timeStamps[current]);
+
+            if (timeDifference == seconds::zero()) throw std::domain_error("Cannot compute maximum rate of descent in m/s when there is travelling of zero seconds duration.");
+
+            metres verticalDifference = routePoints[next].position.elevation() - routePoints[current].position.elevation();
+
+            if (verticalDifference < 0)
+            {
+                speed currentDescentRate = (-verticalDifference) / timeDifference.count();
+                maxDescentRate = std::max(currentDescentRate,maxDescentRate);
+            }
+        }
+    }
+
+    return maxDescentRate;
+}
 
 std::vector<RoutePoint> Track::trackPointsToRoutePoints(std::vector<TrackPoint> trackPoints)
 {
@@ -145,7 +361,5 @@ Track::TimeStamp Track::tmToTimeStamp(std::tm dateTime)
 {
     return std::chrono::system_clock::from_time_t(std::mktime(&dateTime));
 }
-
-///////////////////////////////////////////////////////////////////////////////
 
 }
